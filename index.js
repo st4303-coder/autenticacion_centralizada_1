a297ba9383e6b88ec3f92b57aa141141133f3e7e
DanaZuazo_Personal <danalaurazuazoortiz@gmail.com>
2026-06-29 10:09:24 -0600

    add connection to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5100;
+const connectDB = require('./src/config/database');
+
+connectDB();
 app.listen(PORT, () =>{
     console.log(`Hello world: http://localhost:${PORT}`);
 });
